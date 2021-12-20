515dae3bd395011b238464697d64c1a34c64537a
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2021-12-20 07:23:44 +0700

    Update baitap2.js

--- 111221/baitap2.js
+++ 111221/baitap2.js
@@ -1,1 +1,19 @@
+/**
+ * Bai 2
+ * Viet function countElement(arr){...}
+ * Nhan vao 1 array va in ra xem cac gia tri khac nhau xuat hien bao nhieu lan trong array do
+ * VD: [12,3,4,3,12,5,5,5]
+ * Output format: (Value - Count)
+ * 12 -2
+ * 3 - 2
+ * 4 - 1
+ * 5 - 3
+ */
+const arr = [1, 2, 2, 3, 4, 5, 6, 6, 6, 7, 8, 8, 8, 8, 8, 9, 10;
+const counts = {};
+function COUNTDI{
+for (const num of arr) {
+  counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+}
 
