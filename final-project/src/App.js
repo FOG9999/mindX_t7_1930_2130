cae903ce3bc6115e348dccb3334d606c65f1abd6
Snavy <46926922+Snavy05@users.noreply.github.com>
2022-01-15 21:30:12 +0700

    login page done 80%

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,11 +3,13 @@ import { Routes } from "react-router";
 import OneProduct from "./components/share/one-product";
 import { Route } from "react-router";
 import Login from "./components/login/Login";
+import HomePage from "./components/home-page/HomePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home-page" element={<HomePage />} />
     </Routes>
   );
 }
--- final-project/src/components/login/Login.js
+++ final-project/src/components/login/Login.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class Login extends Component {
-  state = {};
+  state = {
+      username: '',
+      password:'',
+      loggedIn: false,
+  };
+
+  onChangeUsername = (evnt) =>{ 
+    this.setState({ username: evnt.target.value });
+  }
+
+  onChangePassword = (evnt) =>{ 
+    this.setState({ password: evnt.target.value });
+  }
+
+  submit = () => {
+      let fakeUser = JSON.parse(localStorage.getItem('fakeUser'));
+      if (this.state.username !== fakeUser.username){
+          toast("Username does not exist!");
+      }
+      else if (this.state.username === fakeUser.username && this.state.password !== fakeUser.password){
+          toast("Password inputed is wrong!");
+      }
+      else {
+          window.location.href = '/home-page';
+      }
+  }
+
+  componentDidMount() {
+      localStorage.setItem('fakeUser', JSON.stringify({username: 'Snavy', password: '123456'}))
+  }
+
   render() {
     return (
       <div className="container-fluid main d-flex justify-content-center align-items-center">
+        <ToastContainer /> 
         <div className="login-wrapper">
           <div className="header">
             <h4 style={{ fontWeight: 700 }}>Login</h4>
@@ -19,6 +53,8 @@ class Login extends Component {
               className="form-control"
               placeholder="Username"
               id="username"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
             />
           </div>
           <label className="label-input mt-2">Password</label>
@@ -31,6 +67,8 @@ class Login extends Component {
               className="form-control"
               placeholder="Password"
               id="password"
+              value={this.state.password}
+              onChange={this.onChangePassword}
             />
           </div>
           <div className="d-flex justify-content-end">
@@ -40,7 +78,7 @@ class Login extends Component {
               </a>
             </span>
           </div>
-          <div className="my-2 p-2 button-login">LOGIN</div>
+          <div className="my-2 p-2 button-login" onClick={this.submit}>LOGIN</div>
           <div className="text-center mt-5">
             <span style={{ fontWeight: 600 }}>
               Don't have an account? <a href="#">Sign up</a>
