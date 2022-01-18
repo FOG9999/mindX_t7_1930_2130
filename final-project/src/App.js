072bc9f1a547fdafbe4d414d1d7a365073f9a8fc
Snavy <46926922+Snavy05@users.noreply.github.com>
2022-01-18 23:50:46 +0700

    btvn for signup website Snavy

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -4,12 +4,14 @@ import OneProduct from "./components/share/one-product";
 import { Route } from "react-router";
 import Login from "./components/login/Login";
 import HomePage from "./components/home-page/HomePage";
+import SignUpSnavy from "./components/btvn/SignUpSnavy";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home-page" element={<HomePage />} />
+      <Route path="/sign-up" element={<SignUpSnavy />}/>
     </Routes>
   );
 }
--- final-project/src/components/login/Login.js
+++ final-project/src/components/login/Login.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router";
+import { Navigate } from "react-router";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+ 
 class Login extends Component {
   state = {
       username: '',
@@ -27,9 +27,12 @@ class Login extends Component {
           toast("Password inputed is wrong!");
       }
       else {
-          window.location.href = '/home-page';
+        this.setState({
+          loggedIn: true
+        })
       }
   }
+
 
   componentDidMount() {
       localStorage.setItem('fakeUser', JSON.stringify({username: 'Snavy', password: '123456'}))
@@ -39,6 +42,7 @@ class Login extends Component {
     return (
       <div className="container-fluid main d-flex justify-content-center align-items-center">
         <ToastContainer /> 
+        {this.state.loggedIn?<Navigate to="/home-page" />: null}
         <div className="login-wrapper">
           <div className="header">
             <h4 style={{ fontWeight: 700 }}>Login</h4>
@@ -81,7 +85,7 @@ class Login extends Component {
           <div className="my-2 p-2 button-login" onClick={this.submit}>LOGIN</div>
           <div className="text-center mt-5">
             <span style={{ fontWeight: 600 }}>
-              Don't have an account? <a href="#">Sign up</a>
+              Don't have an account? <a href="/sign-up">Sign up</a>
             </span>
           </div>
         </div>
