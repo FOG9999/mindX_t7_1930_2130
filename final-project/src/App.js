994ccce2257efc044bfa4cad1783ca61c6c1df74
Nguyen Thang <nguyenducthang20081999@gmail.com>
2022-01-22 17:05:41 +0700

    signup page

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -4,12 +4,14 @@ import OneProduct from "./components/share/one-product";
 import { Route } from "react-router";
 import Login from "./components/login/Login";
 import HomePage from "./components/home-page/HomePage";
+import Singup from "./components/btvn/thangnd/Signup-thangnd";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home-page" element={<HomePage />} />
+      <Route path="sign-up" element={<Singup />} />
     </Routes>
   );
 }
