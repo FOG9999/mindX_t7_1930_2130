525a45ff4a649be5ae9733d03e21c0b40de0639a
Nguyen Thang <nguyenducthang20081999@gmail.com>
2022-02-12 21:43:32 +0700

    cart view basic done

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import { Routes } from 'react-router';
-import OneProduct from './components/share/one-product';
-import { Route } from 'react-router';
-import Login from './components/login/Login';
-import HomePage from './components/home-page/HomePage';
-import Singup from './components/btvn/thangnd/Signup-thangnd';
+import "./App.css";
+import { Routes } from "react-router";
+import OneProduct from "./components/share/one-product";
+import { Route } from "react-router";
+import Login from "./components/login/Login";
+import HomePage from "./components/home-page/HomePage";
+import Singup from "./components/btvn/thangnd/Signup-thangnd";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/home-page" element={<HomePage />} />
       <Route path="sign-up" element={<Singup />} />
+      <Route path="my-cart" element={<Cart />} />
     </Routes>
   );
 }
--- final-project/src/components/cart/OneCartProduct.js
+++ final-project/src/components/cart/OneCartProduct.js
@@ -1,30 +1,52 @@
-import React from 'react';
+import React from "react";
 
-export default function OneCartProduct(props) {
-  return (
-    <div className="product">
-      <div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZWLAwg4bEAIhAyJ1lgI1F-Xlwx97SThFEsQ&usqp=CAU"
-          className="product-image inline"
-        />
-        <div className="inline">
-          <p>
-            <b>iPhone X 64GB</b>
-          </p>
-          <div>Màu</div>
+export default class OneCartProduct extends React.Component {
+  render() {
+    return (
+      <div className="d-flex my-2" style={{ minWidth: "700px" }}>
+        <div className="d-flex justify-content-center align-items-center">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZWLAwg4bEAIhAyJ1lgI1F-Xlwx97SThFEsQ&usqp=CAU"
+            className="product-image inline"
+            alt=""
+          />
+        </div>
+        <div className="part-two d-flex flex-grow-1">
+          <div className="name-detail flex-grow-1">
+            <h6>{this.props.item.title}</h6>
+            <div className="d-flex">
+              Màu{" "}
+              <span
+                className="selected-circle mx-1"
+                style={{ color: "grey", backgroundColor: "grey" }}
+              ></span>{" "}
+              <span
+                className="circle"
+                style={{ color: "black", backgroundColor: "black" }}
+              ></span>
+            </div>
+          </div>
+          <div className="price" style={{ width: "100px" }}>
+            {this.props.item.price}đ
+          </div>
+          <div className="quantity" style={{ width: "100px" }}>
+            <div className="d-flex">
+              <div className="btn-custom d-flex align-items-center justify-content-center">
+                -
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                value={1}
+                style={{ width: "50px" }}
+              />
+              <div className="btn-custom d-flex align-items-center justify-content-center">
+                +
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <p>29.990.000 đ</p>
-      <div className="no-of-products">
-        <span style={{ padding: '0px 10px' }}>
-          <button>-</button>
-        </span>
-        <p style={{ paddingTop: '3px' }}>1</p>
-        <span style={{ padding: '0px 10px' }}>
-          <button>+</button>
-        </span>
-      </div>
-    </div>
-  );
+    );
+  }
 }
