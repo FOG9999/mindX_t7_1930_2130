d605faf45711e822be0c4b35987ec3286e591e8f
VIETTELGROUP\Thangnd54 <thangnd54>
2022-02-24 16:10:43 +0700

    try to add controller to insert fake data

---
+++ firebase-demo/demo/controllers/user.controller.js
@@ -0,0 +1,18 @@
+const db = require('../database/database');
+const fakeUsers = require('../data.json').message.users;
+
+module.exports = {
+    insertFakeUsers: async (done) => {
+        try {
+            const batch = db.batch();
+            const userRef = db.collection('users');
+            fakeUsers.forEach(user => {
+                batch.create(userRef, user);
+            })
+            const res = await batch.commit();
+            done(res);
+        } catch (error) {
+            done(error);
+        }
+    }
+}
--- firebase-demo/demo/routes/index.js
+++ firebase-demo/demo/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const testController = require("../controllers/test.controller");
+const userController = require("../controllers/user.controller");
 const db = require("../database/database");
 var router = express.Router();
 
@@ -8,5 +9,11 @@ router.get("/", async function (req, res, next) {
   const cities = await testController.getCities(db);
   res.send(cities);
 });
+
+router.post('/insert-fake-user', async (req,res) => {
+  userController.insertFakeUsers((response) => {
+    res.send(response);
+  })
+})
 
 module.exports = router;
