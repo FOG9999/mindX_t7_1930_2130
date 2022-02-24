b7681cc141f6abda9615971018754c6f9e0cecc3
Nguyen Thang <nguyenducthang20081999@gmail.com>
2022-02-24 17:15:45 +0700

    insert successfully fake users

--- firebase-demo/demo/controllers/user.controller.js
+++ firebase-demo/demo/controllers/user.controller.js
@@ -1,18 +1,18 @@
-const db = require('../database/database');
-const fakeUsers = require('../data.json').message.users;
+const db = require("../database/database");
+const fakeUsers = require("../data.json").message.users;
 
 module.exports = {
-    insertFakeUsers: async (done) => {
-        try {
-            const batch = db.batch();            
-            fakeUsers.forEach(user => {
-                const userRef = db.collection('users').doc();
-                batch.create(userRef, user);
-            })
-            const res = await batch.commit();
-            done(res);
-        } catch (error) {
-            done(error);
-        }
+  insertFakeUsers: async (done) => {
+    try {
+      const batch = db.batch();
+      fakeUsers.forEach((user) => {
+        const userRef = db.collection("users").doc();
+        batch.create(userRef, user);
+      });
+      const res = await batch.commit();
+      done(res);
+    } catch (error) {
+      done(error);
     }
-}
+  },
+};
