aa0c93bf5b8ce6db96ab9e4a6f5ad726a5434218
Snavy <46926922+Snavy05@users.noreply.github.com>
2022-02-25 22:55:47 +0700

    component created for product listings

--- final-project/src/draft/snavy/Listings.js
+++ final-project/src/draft/snavy/Listings.js
@@ -5,60 +5,62 @@ import ip11 from './ip11.png'
 import ip12 from './ip12.png'
 import ip13 from './ip13.png'
 
+let cardWrapper = document.getElementsByClassName("cardWrapper")
+
 class Listings extends Component {
   state = {}
   
+
+  displayBlock = () => {
+    document.getElementsByClassName('purchaseButton').style.display = 'block'
+    document.getElementsByClassName('compareButton').style.display = 'block'
+  }
+  
+  // toggleDisplayBtn = () => {
+  //   cardWrapper.addEventListener("mouseover", displayBlock())
+  // }
+
   logOutConsole = () => console.log(product_card)
 
-  // renderBlock = () => {
-  //   return Object.entries(product_card).map((item) =>
-  //     <div className="container">
-  //     <div className="row">
-        
-  //       <div className="col-3">
-  //         <img src={item.image} alt="" className="img-thumbnail img-fluid" />
-  //         <div className="card-body">
-  //           <h5 className="card=title">IPhone X 64GB 12gb RAM</h5>
-  //           <h5 className="badge rounded-pill bg-danger text-white">22.990.000 đ</h5>
-  //           <div className="card-body">
-  //             <h6 className="text-wrap fw-lighter"><i className="fas fa-microchip"></i> A15 Bionic</h6>
-  //             <h6 className="text-wrap fw-lighter"><i className="fas fa-mobile-android-alt"></i> 6.1'</h6>
-  //             <h6 className="text-wrap fw-lighter"><i className="fas fa-memory"></i> 4GB</h6>
-  //             <h6 className="text-wrap fw-lighter"><i className="fas fa-sd-card"></i> 128 GB</h6>
-  //             <div onClick={this.logOutConsole}>Console logs</div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   )
-  // }
+  renderBlock = () => {
+    return product_card.map((item, index) =>
+
+  <div className="col-sm-6 col-md-3 cardWrapper">
+    <div className="card card-container mt-3">
+        <img src={item.image} alt="" className="card-img-top img-fluid"/>
+        <div className="card-body">
+            <h5 className="card=title">{item.product}</h5>
+            <h5 className="badge rounded-pill bg-danger text-white">{item.price}</h5>
+            <div className="card-body d-flex flex-column info-div">
+                <h6 className="text-wrap fw-lighter"><i className="fas fa-microchip"></i> {item.chipset}</h6>
+                <h6 className="text-wrap fw-lighter"><i className="fas fa-mobile-android-alt"></i> {item.screen}</h6>
+                <h6 className="text-wrap fw-lighter"><i className="fas fa-memory"></i> {item.ram}</h6>
+                <h6 className="text-wrap fw-lighter"><i className="fas fa-sd-card"></i> {item.storage}</h6>
+                <a href={"/"}> <img src={item.paymentmethod1} className="payment-method" /></a>
+            </div>
+            <div className="btn-wrapper d-flex flex-col">
+                  <a href="" className="purchaseButton btn btn-danger fw-bolder text-nowrap">Mua ngay</a>
+                  <a href="" className="compareButton btn btn-secondary fw-bolder text-nowrap ">So sánh</a>
+            </div>
+        </div>
+    </div>
+  </div>
+  )
+}
 
   render() {
   return (
-    <div>
-      <div className="container">
-      <div className="row">
-        
-        <div className="col-3">
-          <img src={ip10} alt="" className="img-thumbnail img-fluid" />
-          <div className="card-body">
-            <h5 className="card=title">IPhone X 64GB 12gb RAM</h5>
-            <h5 className="badge rounded-pill bg-danger text-white">22.990.000 đ</h5>
-            <div className="card-body">
-              <h6 className="text-wrap fw-lighter"><i className="fas fa-microchip"></i> A15 Bionic</h6>
-              <h6 className="text-wrap fw-lighter"><i className="fas fa-mobile-android-alt"></i> 6.1'</h6>
-              <h6 className="text-wrap fw-lighter"><i className="fas fa-memory"></i> 4GB</h6>
-              <h6 className="text-wrap fw-lighter"><i className="fas fa-sd-card"></i> 128 GB</h6>
-              <div onClick={this.logOutConsole}>Console logs</div>
-            </div>
-          </div>
+    <div className="container">
+      <div className="row g-4">
+        {this.renderBlock()}
         </div>
       </div>
-    </div>
-    </div>
   )
 }
 }
+
+
+
+ 
 
 export default Listings;
