bd0917f7fe2f81213d2d2d826e00a6cf502e2214
Nguyen Thang <nguyenducthang20081999@gmail.com>
2022-03-04 07:26:17 +0700

    get list product by page, category and key api

--- firebase-demo/demo/routes/product.route.js
+++ firebase-demo/demo/routes/product.route.js
@@ -2,10 +2,17 @@ const productRouter = require("express").Router();
 const productController = require("../controllers/product.controller");
 
 productRouter.post("/insert-fake-product", (req, res, next) => {
-  // request method GET, POST
-  productController.insertFakeProducts((resData) => {
-    res.send(resData);
-  });
+   // request method GET, POST
+   productController.insertFakeProducts((resData) => {
+      res.send(resData);
+   });
+});
+
+productRouter.get("/search", (req, res) => {
+   const { searchKey, category, page } = req.query;
+   productController.getListProducts(searchKey, category, page, (data) => {
+      res.send(data);
+   });
 });
 
 module.exports = productRouter;
