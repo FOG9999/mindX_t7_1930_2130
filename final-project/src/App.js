e2bf1b5ac190bbc20c5b7971ddc71524643ff412
VIETTELGROUP\Thangnd54 <thangnd54>
2022-03-11 11:43:59 +0700

    miss save

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { Routes } from "react-router";
-import OneProduct from "./components/share/one-product";
-import { Route } from "react-router";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import "antd/dist/antd.css";
-import Login from "./components/login/Login";
-import HomePage from "./components/home-page/HomePage";
-import Singup from "./components/btvn/thangnd/Signup-thangnd";
-import Cart from "./components/cart/Cart";
-import Listings from "./draft/snavy/Listings";
-
-function App() {
-   return (
-      <Routes>
-         <Route path="/" element={<Login />} />
-         <Route path="/home-page" element={<HomePage />} />
-         <Route path="/listings" element={<Listings />} />
-         <Route path="sign-up" element={<Singup />} />
-         <Route path="my-cart" element={<Cart />} />
-      </Routes>
-   );
-=======
 import './App.css';
 import { Routes } from 'react-router';
 // import OneProduct from './components/share/one-product';
@@ -41,7 +17,6 @@ function App() {
       <Route path="my-cart" element={<Cart />} />
     </Routes>
   );
->>>>>>> feature/Group2
 }
 
 export default App;
