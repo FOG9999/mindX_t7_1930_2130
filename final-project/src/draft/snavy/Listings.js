6b805ceda48a6ce1881c9485f972856ee1d9ddfa
Snavy <46926922+Snavy05@users.noreply.github.com>
2022-03-16 22:05:32 +0700

    added user check when logged in

--- final-project/src/draft/snavy/Listings.js
+++ final-project/src/draft/snavy/Listings.js
@@ -9,6 +9,7 @@ import { notification } from "antd";
 import { Pagination, Spin } from "antd";
 import { Card } from "antd";
 import { constants } from "../../constants";
+import Header from "../../components/header/Header";
 
 class Listings extends Component {
    state = {
@@ -123,124 +124,12 @@ class Listings extends Component {
    render() {
       return (
          <Spin spinning={this.state.loading} size="default">
+            <Header />
             <div className="container">
-               <div className="alert alert-primary alert-dismissible fade show" role="alert">
-                  <h4 className="alert-heading">
-                     Sale 50%! <span className="badge badge-primary">All New Products</span>{" "}
-                  </h4>
-                  <p>
-                     Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind
-                     of content
-                  </p>
-                  <hr />
-                  <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-                  <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                     <span aria-hidden="true">&times;</span>
-                  </button>
-               </div>
-               <nav className="navbar navbar-expand-xl navbar navbar-dark bg-dark text-white w-100 p-3">
-                  <a className="navbar-brand" href="#">
-                     NCT-JSI01
-                  </a>
-                  <button
-                     className="navbar-toggler"
-                     type="button"
-                     data-toggle="collapse"
-                     data-target="#navbarSupportedContent"
-                     aria-controls="navbarSupportedContent"
-                     aria-expanded="false"
-                     aria-label="Toggle navigation"
-                  >
-                     <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item dropdown">
-                           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Điện thoại
-                           </a>
-                           <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a className="dropdown-item" href="#">
-                                 Action
-                              </a>
-                              <a className="dropdown-item" href="#">
-                                 Another action
-                              </a>
-                              <div className="dropdown-divider"></div>
-                              <a className="dropdown-item" href="#">
-                                 Something else here
-                              </a>
-                           </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Máy tính
-                           </a>
-                           <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a className="dropdown-item" href="#">
-                                 Action
-                              </a>
-                              <a className="dropdown-item" href="#">
-                                 Another action
-                              </a>
-                              <div className="dropdown-divider"></div>
-                              <a className="dropdown-item" href="#">
-                                 Something else here
-                              </a>
-                           </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Phụ kiện điện tử
-                           </a>
-                           <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a className="dropdown-item" href="#">
-                                 Action
-                              </a>
-                              <a className="dropdown-item" href="#">
-                                 Another action
-                              </a>
-                              <div className="dropdown-divider"></div>
-                              <a className="dropdown-item" href="#">
-                                 Something else here
-                              </a>
-                           </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Máy tính
-                           </a>
-                           <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a className="dropdown-item" href="#">
-                                 Action
-                              </a>
-                              <a className="dropdown-item" href="#">
-                                 Another action
-                              </a>
-                              <div className="dropdown-divider"></div>
-                              <a className="dropdown-item" href="#">
-                                 Something else here
-                              </a>
-                           </div>
-                        </li>
-                     </ul>
-                     <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                     </form>
-                  </div>
-                  <div className="text-end">
-                     <button type="button" className="btn btn-outline-light mr-2">
-                        Đăng nhập
-                     </button>
-                     <button type="button" className="btn btn-warning">
-                        Đăng kí
-                     </button>
-                  </div>
-               </nav>
-               <div className="row g-4">{this.renderBlock()}</div>
-               <div className="d-flex justify-content-center mt-5 mb-2">
-                  <Pagination current={this.state.currentPage} defaultCurrent={1} total={this.state.total} pageSize={constants.PAGE_SIZE} onChange={this.onPageChange} />
-               </div>
+            <div className="row g-4">{this.renderBlock()}</div>
+            <div className="d-flex justify-content-center mt-5 mb-2">
+               <Pagination current={this.state.currentPage} defaultCurrent={1} total={this.state.total} pageSize={constants.PAGE_SIZE} onChange={this.onPageChange} />
+            </div>
             </div>
          </Spin>
       );
