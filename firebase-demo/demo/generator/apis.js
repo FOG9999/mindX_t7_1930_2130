177604fe24dd74a739296058f5c98ea54c1ea7d7
VIETTELGROUP\Thangnd54 <thangnd54>
2022-03-16 11:22:42 +0700

    function to get images from shopee

---
+++ firebase-demo/demo/generator/apis.js
@@ -0,0 +1,4 @@
+module.exports = {
+    SHOPEE_LIST_PRODUCTS: 'https://shopee.vn/api/v4/search/search_items?by=relevancy&categoryids=101934%2C101941&limit=60&match_id=11035954&newest=0&order=desc&page_type=search&scenario=PAGE_OTHERS&version=2',
+    SHOPEE_IMAGE_SERVER: 'https://cf.shopee.vn/file/'
+}
---
+++ firebase-demo/demo/generator/update-products.js
@@ -0,0 +1,21 @@
+const shopeeProducts = require('./shopee-products.json').items;
+let productImages = [];
+shopeeProducts.forEach(pro => {productImages = [...productImages, ...pro.item_basic.images]})
+// console.log(productImages);
+
+const getRandomImages = () => {
+    let numOfImages = Math.round(Math.random()*5)+5;
+    let output = [];
+    for(let i=0; i<numOfImages; i++){
+        let randomIndex = Math.round(Math.random()*productImages.length);
+        output.push(productImages[randomIndex]);
+    }
+    return output;
+}
+
+console.log(getRandomImages())
+
+module.exports = { 
+    productImages,
+    getRandomImages
+}
